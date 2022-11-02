d0fae97b81d515fd7efdc851dbd52d3f3ba10ebd
Worameth Semapat <worameth@ookbee.com>
2022-11-02 11:56:45 +0700

    feat: svg qrcode

---
+++ README.md
@@ -0,0 +1,1 @@
+#qrcoder-dotnet-playground
---
+++ src/MySvgQrCode/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Reflection;
+using QRCoder;
+
+var qrGenerator = new QRCodeGenerator();
+var qrCodeData = qrGenerator.CreateQrCode("http://www.example.com", QRCodeGenerator.ECCLevel.Q);
+var svgQrCode = new SvgQRCode(qrCodeData);
+var qrCode = svgQrCode.GetGraphic(20);
+
+// Console.WriteLine(qrCode);
+
+var rootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!;
+var svgPath = Path.Combine(rootPath, "example.svg");
+File.WriteAllText(svgPath, qrCode);
+
+Console.WriteLine($"Generate SVG File to: {svgPath}");
